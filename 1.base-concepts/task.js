43c981bc7aaaf0df7e86e0f18c2dbd674c07c4ba
PetrDotsenko <88041442+PetrDotsenko@users.noreply.github.com>
2023-09-06 21:18:48 +1000

    changed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const discriminant = b**2 - 4 * a * c;
+  if (discriminant > 0) {
+    let root1 = (-b + Math.sqrt(discriminant) )/(2*a);
+    let root2 = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push(root1, root2);
+  }
+  else if (discriminant === 0) 
+  {
+    let root = -b/(2*a);
+    arr.push(root);
+  } else {
+    return arr;
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12;
+  let mainDebt = amount - contribution;
+  let payPerMounth = mainDebt * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let overpayment = payPerMounth  * countMonths;
+  overpayment = +((overpayment).toFixed(2));
+  return overpayment;
 }
