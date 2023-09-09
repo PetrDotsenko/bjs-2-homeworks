937d4d7885a028bf9b3226040ae98f42b93eac09
PetrDotsenko <88041442+PetrDotsenko@users.noreply.github.com>
2023-09-09 19:07:35 +1000

    changed task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,27 @@
-function getArrayParams(...arr) {
+// ZADANIE 1
 
+function getArrayParams(...arr) {
+  let max = -Infinity;
+  let min = Infinity;
+  let sum;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max === arr[i];
+    }
+    if (arr[i] < min) {
+      min === arr[i];
+    }
+    sum += arr[i];
+  }
+  let avg = sum / arr.length;
+  avg = +((avg).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
+
+
+
+// ZADANIE 2
 function summElementsWorker(...arr) {
 
 }
