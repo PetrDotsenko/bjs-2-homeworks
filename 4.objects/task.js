51a97a14c6abff74fb49dc51c275472345646c9d
PetrDotsenko <88041442+PetrDotsenko@users.noreply.github.com>
2023-09-19 21:14:00 +1000

    change

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,13 +1,21 @@
 function Student(name, gender, age) {
-  
+    this.name = name; 
+    this.gender = gender; 
+    this.age = age;
+    this.marks = [];  
 }
+new Student("Maks", "male", "17");
+new Student("Gena", "male", "18");
+new Student("Masha", "female", "15");
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  //по инструкции к домашнему заданию весь пункт два уже был выполнен
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (marks in Student) {
+    addMarks(...marksToAdd).push.Student.marks;
+    }
 }
 
 Student.prototype.getAverage = function () {
